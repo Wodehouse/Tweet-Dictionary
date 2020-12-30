8581aa482dd122478fc7cc2b727c5d49baf82e60
Dorothy Najjuma Kamya <68673729+Wodehouse@users.noreply.github.com>
2020-12-30 16:29:52 -0500

    Update hashFunctions.h

--- Tweets Dictionary/hashFunctions.h
+++ Tweets Dictionary/hashFunctions.h
@@ -1,12 +1,8 @@
 #pragma once
 
 /*
-  Copyright (c) 2020
-  Swarthmore College Computer Science Department, Swarthmore PA
-  J. Brody, A. Danner, M. Gagne, L. Meeden, Z. Palmer, A. Soni, M. Wehar
-  Distributed as course material for Fall 2020
-  CPSC 035: Data Structures and Algorithms
-  https://tinyurl.com/yyr8mdoh
+  Wodehouse
+  hash functions file
 */
 
 #include <string>
